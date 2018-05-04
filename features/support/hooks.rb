3029de9d44ffcd819b63dbf729aed85f6ef8f152
Adityaridha <adityaridharrahman@gmail.com>
2018-05-04 15:40:00 +0700

    add screen capture functionality

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,15 @@
+#Create a directory for storing snapshot
+# dir_path = "/home/vchouhan/vijay_work/snapshot"
+# unless Dir.exist?(dir_path)
+#     Dir.mkdir(dir_path,0777)
+#     puts "=========Directory is created at #{dir_path}"
+# else
+#     puts "=========Directory is exist at #{dir_path}"
+# end
+
 After do |scenario|
+
+    page.save_screenshot('image_cucumber.png')
 
     visit('https://mentifi.hub3c.com/Account/SignOut')
     
